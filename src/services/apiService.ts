c84b1d966fb0d99b6a61b099ac9d068b1b99b412
Guilherme Ferreira <guilhermefreichert@gmail.com>
2026-04-08 14:19:52 -0300

    Feat: projeto finalizado

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 
-  baseURL: 'http://localhost:3333', 
+  baseURL: 'https://growtwitter-react-mu.vercel.app/', 
 });
 
 api.interceptors.request.use((config) => {
